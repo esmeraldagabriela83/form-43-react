148ebdd6e873499b5c3575537f5a81bd5e03a104
Acornicesei Mihaela Gabriela <esmeraldagabriela83@yahoo.com>
2022-09-04 21:54:36 +0300

    zacusca class and constructor function

--- src/js/constructor.js
+++ src/js/constructor.js
@@ -182,6 +182,15 @@ es6Car.go(39);
 es6Car.show();
 
 console.log("--- es6Car is : " , es6Car);
+// ES6Car
+// name: "Dacia"
+// speed: 89
+// [[Prototype]]: Object
+// constructor: class ES6Car
+// go: ƒ go(plus)
+// show: ƒ show()
+// [[Prototype]]: Object
+
 //mostenire [inheritance]
 class Truck extends ES6Car {
     //implicit mosteneste consturctorul
@@ -212,6 +221,7 @@ truck.show();
 
 console.log("--- truck is : " , truck);
 //--- truck is :  Truck {speed: 20, name: 0, weight: 3}
+//la truck name este 0 pt ca super a anulat valoara parametrului real "Scania" ce ara pt parametrul formal name al lui ES6Car
 
 class Suv extends ES6Car {
   //pot adauga proprietati noi
@@ -233,6 +243,7 @@ suv.show();
 
 console.log("--- suv Is : " , suv);
 //--- suv Is :  Suv {speed: 132, name: 'X5'}
+//suv are ca name pe X5 pt ca nu are un super care sa anuleze valoarea parametrului formal name al lui ES6Car
 
 ///NOTIUNEA DE CLASA ABSTRACTA
 
@@ -244,7 +255,111 @@ console.log("--- suv Is : " , suv);
 //https://www.codegrepper.com/search.php?q=class%20javascript
 //https://www.w3schools.com/js/js_asynchronous.asp
 
+//------------------------------------------------------------------------------
+//https://www.w3schools.com/js/js_classes.asp
 
+class ClassCar{
+  constructor(name , year){
+        //aici se declara/initializeaza proprietatile
+    this.name=name;
+    this.year=year;
+  }
+  //metode
+  age(){
+    let date=new Date();
+
+    return date.getFullYear() - this.year;
+  }
+}
+
+let schoolCar=new ClassCar("Ford", 2014);
+
+console.log("--- schoolCar is : " , schoolCar);
+//--- schoolCar is :
+// Car {speed: 'Ford', name: 2014, go: ƒ}
+// go: ƒ (plus)
+// name: 2014
+// speed: "Ford"
+// [[Prototype]]: Object
+
+document.getElementById('schoolCar').innerText="My car name is : " +  schoolCar.name +" . " + " It is " + schoolCar.age() + " years old.";
+
+//------------------------------------------------------------------------------
+//https://www.w3schools.com/js/js_classes.asp
+//https://www.codegrepper.com/search.php?q=class%20javascript
+
+//class genereaza obiecte
+
+class FoodAutumn{
+  constructor(ingredient1, ingredient2){
+      //aici se declara/initializeaza proprietatile
+    this.ingr1=ingredient1;
+    this.ingr2=ingredient2;
+  }
+  //metode
+  makeZacusca(year){
+    return `This year : ${year}, I make zacusca with ${this.ingr1} and ${this.ingr2} .`
+  }
+}
+
+let zacusca1=new FoodAutumn("ceapa" , "morcovi");
+console.log("zacusca1 is : " , zacusca1);
+// zacusca1 is :
+// FoodAutumn {ingr1: 'ceapa', ingr2: 'morcovi'}
+// ingr1: "ceapa"
+// ingr2: "morcovi"
+// [[Prototype]]: Object
+// constructor: class FoodAutumn
+// makeZacusca: ƒ makeZacusca(year)
+// [[Prototype]]: Object
+console.log("zacusca1 este : " , typeof(zacusca1));//zacusca1 este :  object
+
+document.getElementById('zacusca1Paragraph').innerText=zacusca1.makeZacusca(2022);
+
+let zacusca2=new FoodAutumn("cartofi" , "telina");
+console.log("zacusca2 is : " , zacusca2);
+document.getElementById('zacusca2Paragraph').innerText=zacusca2.makeZacusca(2000);
+
+let zacusca3=new FoodAutumn("ardei" , "rosii");
+console.log("zacusca3 is : " , zacusca3);
+document.getElementById('zacusca3Paragraph').innerText=zacusca3.makeZacusca(1983);
+
+//------------------------------------------------------------------------------
+//functia constructor genereaza obiecte
+
+function AutumnFuncZacusca(ingredient1, ingredient2){
+  this.ingred1=ingredient1;
+  this.ingred2=ingredient2;
+  this.makeAutumnZacusca=function(year){
+    return "Zacusca mea este facuta din " + this.ingred1 + " and " + this.ingred2 + " ,ingredients which  are picked in " + year + " year.";
+  }
+}
+
+let zacuscaAutumn1=new AutumnFuncZacusca("castraveti" , "sfecla rosie");
+console.log("zacuscaAutumn1 is " , zacuscaAutumn1);
+// zacuscaAutumn1 is
+// AutumnFuncZacusca {ingred1: 'castraveti', ingred2: 'sfecla rosie', makeAutumnZacusca: ƒ}
+// ingred1: "castraveti"
+// ingred2: "sfecla rosie"
+// makeAutumnZacusca: ƒ (year)
+// [[Prototype]]: Object
+console.log("zacuscaAutumn1 este de tip : " , typeof(zacuscaAutumn1));//zacuscaAutumn1 este de tip :  object
+
+document.getElementById('zacuscaAutumnParagraph1').innerText=zacuscaAutumn1.makeAutumnZacusca(1985);
+//
+let zacuscaAutumn2=new AutumnFuncZacusca("frunze de dafin" , "sare");
+console.log("zacuscaAutumn2 is :" , zacuscaAutumn2);
+document.getElementById('zacuscaAutumnParagraph2').innerText=zacuscaAutumn2.makeAutumnZacusca(2003);
+//
+let zacuscaAutumn3=new AutumnFuncZacusca("piper" , "usturoi");
+console.log("zacuscaAutumn3 is :" , zacuscaAutumn3);
+document.getElementById('zacuscaAutumnParagraph3').innerText=zacuscaAutumn3.makeAutumnZacusca(1995);
+
+//https://www.udemy.com/course/modern-javascript-es6-for-react-js/
+//https://www.codegrepper.com/search.php?q=class%20javascript
+
+//https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+//Prototype methods
 
 
 
